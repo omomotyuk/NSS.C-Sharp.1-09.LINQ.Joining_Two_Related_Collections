9adf51ff425c7a0704e7119370c9a52d2956ffac
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-21 09:56:19 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_09.LINQ.Joining_Two_Related_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-09.LINQ.Joining_Two_Related_Collections
